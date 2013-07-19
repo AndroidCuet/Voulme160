f55107b59c74886c5d46fb8f791660da34d77b4b
Ratna Halder <ratna@mpower-health.com>
2013-07-20 00:02:24 +0600

    code is now working but cant upload

--- src/Main.java
+++ src/Main.java
@@ -2,97 +2,68 @@
 import java.util.HashMap;
 import java.util.Scanner;
 
-
 public class Main {
 	
-	static class Factorization{
-		
-	private long finalPermutation = 1;
+	static class Factorization{		
+
 	private int input;
 	
 	private HashMap<String, Integer> primeFactorLists = new HashMap<String, Integer>();
 	private long[] prime = {2,3,5,7,11,13,17,19,23,29,31,37,41,43,47,53,59,61,67,71,73,79,83,89,97,101}; 
+	
 	private void takeInput(){
 		
 		try{
 		Scanner in = new Scanner(System.in);
 		do{
 			input = in.nextInt();
+			if(input != 0){
+			primeFactorLists.clear();
 			
-			clearAllData();
-			getFactorial(input);
-			getPrimeFactors(finalPermutation);	
+			getPrimeFactors(input);	
 			showOutput();
+			}
 		}
 		while(input !=0 );
+		
 		}catch(NumberFormatException e){
 			e.printStackTrace();
 		}
 	}
 	
-	private void clearAllData(){
-		finalPermutation = 1;
-		primeFactorLists.clear();
-	}
-	private void getFactorial(int currentFactor){
+	private void getPrimeFactors(int input){
 		
-		if(currentFactor == 1){
-			return;
-		}else{
-			finalPermutation = finalPermutation * currentFactor;
-			getFactorial(currentFactor - 1);
-		}				
-	}
-	
-	private void getPrimeFactors(long value){
-		if(value == 1){
-			return;
-		}
-		
-		if(value % 2 == 0){
+		for(int i = 0; prime[i]<=input ; i++){
 			
-			synchronized (primeFactorLists) {
+			long calculatingFactor = prime[i];
+			int numberOfFactor = 0;
+			
+			while(input/calculatingFactor >= 1){
 				
-			Integer count = primeFactorLists.get("2");
-			primeFactorLists.put("2", count == null ? 1:count + 1);
-			
-			}
-		
-			getPrimeFactors(value / 2);
-			
-		}else{
-			for(int i = 3; i<= value; i = i + 2){
-			
-				if(value % i == 0){
-					synchronized (primeFactorLists) {
-						
-						Integer count = primeFactorLists.get(String.valueOf(i));
-						primeFactorLists.put(String.valueOf(i), count == null ? 1:count + 1);
-						getPrimeFactors(value / i);
-						break;
-					}						
-				}
+				numberOfFactor += input/calculatingFactor;
+				calculatingFactor *= prime[i];			
 			}
 			
-		}
+			primeFactorLists.put(String.valueOf(prime[i]), numberOfFactor);
+		}// end for loop
+				
 	}
 	
 	
 	private void showOutput(){
-	
-		for(int i = 0, j = 0; i < primeFactorLists.size(); j++){
-			
-			if(primeFactorLists.get(String.valueOf(prime[j])) != null){
-				System.out.print(primeFactorLists.get(String.valueOf(prime[j])) + " ");
-				i += 1;
-			}else{
-				System.out.print("0" + " ");
-			}
+		
+		System.out.print(input + "! =");
+		
+		for(int i = 0, j = 0; i < primeFactorLists.size(); i++, j++){		
 			
 			if(j == 15){
 				System.out.print("\n");
+				j = 0;
 			}
-		}
+			
+		System.out.print(primeFactorLists.get(String.valueOf(prime[i])) + " ");				
+						
+		}//end for loop
 	}
 	
 }
